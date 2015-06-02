0d3c5c2249a09e65c1efee38d7469c016dc1a3c8
Daniel <Daniel@DANI>
2015-06-02 09:23:45 +0200

    commitFinal

---
+++ src/superficies/Superficies.java
@@ -0,0 +1,31 @@
+package superficies;
+import superficies.Modelo;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import superficies.Visualizar;
+
+/**
+ * Examen de MVC 3ª Avaliación
+ * ramaCod examen
+ * @author damian
+ */
+public class Superficies {
+
+
+    /**cambios realizados
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //pedir fig
+        //Pedir dato
+        //calcularDato
+        //imrpimir
+        Modelo contenedor= new Modelo();
+        Visualizar.menu(contenedor);
+    }
+    
+    
+
+    
+    
+}
---
+++ test/superficies/LogicTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superficies;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class LogicTest {
+    
+    public LogicTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testCalcular() {
+        System.out.println("calcular");
+        Modelo contenedor =new Modelo(0, 25, 5, 0, 0, 0, 0, 0, 0, 0);
+        float expResult = 25.0F;
+        float result = Logic.calcular(contenedor);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+    
+}
